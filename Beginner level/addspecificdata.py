aea2f82113fc97f56effc57fb0a1fd5943c15fc7
Thuvajarohana <37337110+Thuvajarohana@users.noreply.github.com>
2018-03-20 16:08:42 +0530

    Update addspecificdata.py

--- Beginner level/addspecificdata.py
+++ Beginner level/addspecificdata.py
@@ -1,8 +1,10 @@
 total=int(raw_input())
 req=int(raw_input())
-output=0
-for i in range(1,total+1):
-	m=int(raw_input())
-for j in range(1,req+1):
-	output=output+j
-print(output)
+m=[]
+sum=0
+for i in range(0,total):
+	k=int(raw_input())
+	m.append(k)
+for j in range(0,req):
+	sum+=m[j]
+print(sum)
