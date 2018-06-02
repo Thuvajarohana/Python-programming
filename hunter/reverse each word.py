c3f29be91bf76c1ecd35d77d5a8f26e597cd7a1a
Thuvajarohana <37337110+Thuvajarohana@users.noreply.github.com>
2018-06-02 21:01:23 +0530

    Update reverse each word.py

--- hunter/reverse each word.py
+++ hunter/reverse each word.py
@@ -1,7 +1,10 @@
-n=raw_input()
-k=n.split()
-l=[]
-for j in k:
-  l.append(j[::-1])
-str1 = ' '.join(l)
-print str1
+S=raw_input()
+N=len(S)
+if(1<=N<=100000):
+  k=S.split()
+  l=[]
+  for j in k:
+    l.append(j[::-1])
+  str1 = ' '.join(l)
+  print str1
+
